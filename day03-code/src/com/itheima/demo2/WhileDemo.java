97a82c89f4cc3dc4f3747b3f186a3bc18f90ad16
ChenJunchao <junchao.chan@outlook.com>
2025-12-02 00:30:09 +0800

    feat(demo2): 添加猜数字游戏功能
    - 导入 Random 和 Scanner 类以支持随机数生成和用户输入
    - 实现了一个简单的猜数字游戏逻辑
    - 游戏会提示用户输入的数字是偏大还是偏小，并在猜对时给出 congratulation

--- day03-code/src/com/itheima/demo2/WhileDemo.java
+++ day03-code/src/com/itheima/demo2/WhileDemo.java
@@ -1,5 +1,8 @@
 package com.itheima.demo2;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class WhileDemo {
     public static void main(String[] args) {
         int a = 102;
@@ -18,6 +21,22 @@ public class WhileDemo {
         }
         System.out.println(a);
         System.out.println(count);
+
+        Random random = new Random();
+        int number = random.nextInt(10)+1;
+        Scanner sc = new Scanner(System.in);
+        int count1 = sc.nextInt();
+        while(number!=count1){
+            System.out.println("请输入数字：");
+            if (count1<number) {
+                System.out.println("数字小了");
+            } else if (count1>number) {
+                System.out.println("数字大了");
+            }else {
+                System.out.println("恭喜你猜对了");
+            }
+            count1 = sc.nextInt();
+        }
 
     }
 }
