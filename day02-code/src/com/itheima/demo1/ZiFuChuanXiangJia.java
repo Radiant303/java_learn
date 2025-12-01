0ed19163fe56b31aa94d60b3285307e871eaa2f0
ChenJunchao <junchao.chan@outlook.com>
2025-12-01 21:42:04 +0800

    1

--- day02-code/src/com/itheima/demo1/ZiFuChuanXiangJia.java
+++ day02-code/src/com/itheima/demo1/ZiFuChuanXiangJia.java
@@ -7,7 +7,7 @@ public class ZiFuChuanXiangJia {
         Scanner  sc = new Scanner(System.in);
         int num = sc.nextInt();
         System.out.println("个位是: "+num%10);
-        System.out.println("十位是:"+num/10%10);
+        System.out.println("十位是: "+num/10%10);
         System.out.println("百位是: "+num/100%10);
     }
 }
