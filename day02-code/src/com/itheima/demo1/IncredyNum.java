5d9d443a585330b86b19dc326032bbd693918a50
ChenJunchao <junchao.chan@outlook.com>
2025-12-01 22:21:17 +0800

    feat(day02-code): 添加新的 IncredyNum 类并修改 ZiFuChuanXiangJia 类
    - 在 com.itheima.demo1 包下新增 IncredyNum 类，演示自增自减运算符的使用
    - 修改 ZiFuChuanXiangJia 类中的输出语句，移除数字前的冗余字符 "1"

---
+++ day02-code/src/com/itheima/demo1/IncredyNum.java
@@ -0,0 +1,13 @@
+package com.itheima.demo1;
+
+public class IncredyNum {
+    public static void main(String[] args) {
+        //自增自减运算符
+        int a = 10;
+        a++;
+        System.out.println(a);
+        a--;
+        System.out.println(a);
+
+    }
+}
--- day02-code/src/com/itheima/demo1/ZiFuChuanXiangJia.java
+++ day02-code/src/com/itheima/demo1/ZiFuChuanXiangJia.java
@@ -6,8 +6,8 @@ public class ZiFuChuanXiangJia {
     public static void main(String[] args) {
         Scanner  sc = new Scanner(System.in);
         int num = sc.nextInt();
-        System.out.println("1个位是: "+num%10);
-        System.out.println("1十位是: "+num/10%10);
-        System.out.println("1百位是: "+num/100%10);
+        System.out.println("个位是: "+num%10);
+        System.out.println("十位是: "+num/10%10);
+        System.out.println("百位是: "+num/100%10);
     }
 }
