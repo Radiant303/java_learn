0b0ab642d1be9e7e0f22722b93100d467dcc1ed7
ChenJunchao <junchao.chan@outlook.com>
2025-12-01 22:27:33 +0800

    feat(day02-code): 添加赋值运算符的使用示例
    - 在 demo1 包中新增 FuZhiYunSuanFu 类
    - 演示了 += 赋值运算符的使用方法
    - 提及了 += -= *= /= %= 运算符的隐含强制类型转换特性

---
+++ day02-code/src/com/itheima/demo1/FuZhiYunSuanFu.java
@@ -0,0 +1,17 @@
+package com.itheima.demo1;
+
+public class FuZhiYunSuanFu {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        a += b;        //等同a = a + b
+        System.out.println(a);
+
+        //细节:
+        // += -+ *= /= %= 隐含强制类型转行
+
+
+
+
+    }
+}
