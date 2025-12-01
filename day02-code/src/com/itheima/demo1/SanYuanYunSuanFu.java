99b3404cb19d732ffa40a312c8e23d80faaecfbb
ChenJunchao <junchao.chan@outlook.com>
2025-12-01 23:09:50 +0800

    feat(day03-code): 添加三元运算符示例
    - 在 day03-code 模块中添加 SanYuanYunSuanFu 类
    - 演示了如何使用三元运算符进行条件判断
    - 新增模块文件 day03-code.iml，配置项目模块信息

---
+++ day02-code/src/com/itheima/demo1/SanYuanYunSuanFu.java
@@ -0,0 +1,9 @@
+package com.itheima.demo1;
+
+public class SanYuanYunSuanFu {
+    public static void main(String[] args) {
+        //三元运算符
+        int x = 10>8?10:8;
+        System.out.println(x);
+    }
+}
