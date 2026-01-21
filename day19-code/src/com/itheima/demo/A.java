f82cae1487202fda0bc5d5fafbe2a330159c9c2f
ChenJunchao <junchao.chan@outlook.com>
2026-01-21 21:15:02 +0800

    feat(java): 添加基础Java类A演示字符串操作
    - 创建了com.itheima.demo.A类
    - 实现main方法进行字符串变量赋值操作
    - 演示了字符串从"LOVE"到"LOVE2"的修改过程
    - 添加了控制台输出功能

---
+++ day19-code/src/com/itheima/demo/A.java
@@ -0,0 +1,10 @@
+package com.itheima.demo;
+
+public class A {
+    public static void main(String[] args) {
+        String name = "LOVE";
+        name = "LOVE2";
+        System.out.println(name);
+    }
+
+}
