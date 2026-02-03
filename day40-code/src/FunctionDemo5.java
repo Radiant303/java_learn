544bbae15f495ee08f3672b9d80908643f4b9cb1
ChenJunchao <junchao.chan@outlook.com>
2026-02-04 00:25:36 +0800

    feat(stream): 添加函数式编程示例代码
    - 实现了 FunctionDemo5 类用于演示 Stream 操作
    - 创建字符串列表并使用 Stream 进行数据转换
    - 应用 map 操作将字符串转换为大写格式
    - 使用 forEach 输出转换后的结果到控制台
    - 集成了 ArrayList 和 Collections 工具类的使用示例

---
+++ day40-code/src/FunctionDemo5.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class FunctionDemo5 {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        Collections.addAll(list,"acs","bcd","cde","def","efg","fgh","ghi","hij","ijk","jkl","klm","lmn","mno","nop","opq","pqr","qrs","rst","stu","tuv","uvw","vwx","wxy","xyz");
+        list.stream().map(String::toUpperCase).forEach(System.out::println);
+    }
+}
