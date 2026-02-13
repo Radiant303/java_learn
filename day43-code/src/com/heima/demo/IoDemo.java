95188764a53743574506cb4a071bd93432b47785
ChenJunchao <junchao.chan@outlook.com>
2026-02-13 23:53:47 +0800

    feat(io): 添加文件输出流功能实现
    - 导入 FileOutputStream 和 IOException 类
    - 在 main 方法中添加 throws IOException 声明
    - 创建 FileOutputStream 对象写入文件 day43-code\a.txt
    - 写入字节数据 97 和 98 到文件
    - 关闭文件输出流资源
    - 创建 a.txt 文件并写入 ab 内容

--- day43-code/src/com/heima/demo/IoDemo.java
+++ day43-code/src/com/heima/demo/IoDemo.java
@@ -1,7 +1,15 @@
 package com.heima.demo;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class IoDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        //1.创建一个输出流对象
+        FileOutputStream fos = new FileOutputStream("day43-code\\a.txt");
+        fos.write(97);
+        fos.write(98);
+        fos.close();
 
     }
 }
