93a5c97a793a173dd0bf611bc3f51332b22b52f5
ChenJunchao <junchao.chan@outlook.com>
2026-02-15 23:24:10 +0800

    feat(io): 添加文件写入功能并修改测试文件
    - 创建 IoDemo2 类实现文件输出流功能
    - 将字节数组 {97,98,99} 写入到 day43-code\a.txt 文件
    - 添加字符串 "hello world" 写入功能
    - 在文件中添加换行符并写入测试内容
    - 修改 a.txt 文件内容从 "ab" 变为 "bc" 和 "hello world"

---
+++ day43-code/src/com/heima/demo/IoDemo2.java
@@ -0,0 +1,24 @@
+package com.heima.demo;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class IoDemo2 {
+    public static void main(String[] args) throws IOException {
+        FileOutputStream fos = new FileOutputStream("day43-code\\a.txt");
+
+        byte[] bytes = {97,98,99};
+        fos.write(bytes,1,2);
+
+        String str = "hello world";
+        byte[] bytes1 = str.getBytes();
+
+        String wrap = "\r\n";
+        byte[] bytes2 = wrap.getBytes();
+        fos.write(bytes2);
+        fos.write(bytes1);
+
+        fos.close();
+
+    }
+}
